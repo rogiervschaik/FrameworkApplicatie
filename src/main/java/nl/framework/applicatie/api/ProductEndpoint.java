d28f525e3565324703cbacb6b9b5763cfc2e8b96
Rogier van Schaik <rogiervschaik@hotmail.com>
2023-07-25 14:01:45 +0200

    initial commit

--- src/main/java/nl/framework/applicatie/api/ProductEndpoint.java
+++ src/main/java/nl/framework/applicatie/api/ProductEndpoint.java
@@ -3,9 +3,7 @@ package nl.framework.applicatie.api;
 import nl.framework.applicatie.domein.Product;
 import nl.framework.applicatie.persist.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -15,22 +13,56 @@ public class ProductEndpoint {
     @Autowired
     ProductService ps;
 
-    @GetMapping("Product1")
-    public String MijnMethode(){
+    @GetMapping("prijs")
+    public String prijs() {
         System.out.println("Deze methode wordt aangeroepen");
-        return "tekst";
+        return "alle prijzen";
     }
 
-    @GetMapping("Product2")
-    public String MijnMethode2(){
+    @GetMapping("categorie")
+    public String categorie() {
         System.out.println("Dit is een andere methode");
-        return "tekst";
+        return "alle categories";
     }
-    @GetMapping("product3/{mijnvar}")
-    public String MijnMethode3(@PathVariable("mijnvar") String onzevar){
+
+    @GetMapping("leverancier")
+    public String leverancier() {
+        System.out.println("Dit is een andere methode");
+        return "alle leveranciers";
+    }
+
+    @GetMapping("productfoto")
+    public String productfoto() {
+        System.out.println("Dit is een andere methode");
+        return "alle productfotos";
+    }
+
+    @GetMapping("productbeschrijving")
+    public String productbeschrijving() {
+        System.out.println("Dit is een andere methode");
+        return "alle productbeschrijvingen";
+    }
+
+    @GetMapping("beoordeling")
+    public String beoordeling() {
+        System.out.println("Dit is een andere methode");
+        return "alle beoordelingen";
+    }
+
+    @GetMapping("producten/{mijnvar}/{mijnvar2}")
+    public String MijnMethode3(@PathVariable("mijnvar") String onzevar) {
         System.out.println("Dit is de derde methode" + onzevar);
         return "tekst";
     }
 
+    @GetMapping("alleproducten")
+    public Iterable<Product> alleProducten() {
+        System.out.println("Dit is een andere methode");
+        return ps.geefAlleProducten();
+    }
+    @PostMapping("voegproductentoe")
+    public void voegProductenToe(@RequestBody Product product){
+        System.out.println(product.getLeverancier());
+    }
 }
 
