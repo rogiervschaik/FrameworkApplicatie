55561bf3c5b7aee23f7c530b98616b1730fd6af5
Rogier van Schaik <rogiervschaik@hotmail.com>
2023-08-09 11:08:24 +0200

    laatste commit

--- src/main/java/nl/framework/applicatie/api/AccountEndpoint.java
+++ src/main/java/nl/framework/applicatie/api/AccountEndpoint.java
@@ -4,9 +4,7 @@ package nl.framework.applicatie.api;
 import nl.framework.applicatie.domein.Account;
 import nl.framework.applicatie.persist.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountEndpoint {
@@ -17,5 +15,24 @@ public class AccountEndpoint {
     @GetMapping ("accounts")
     public Iterable<Account> methodeVindAccounts() {
         return as.vindAlleAccounts();
+    }
+    @PostMapping("accounts")
+    public Account saveAccount(@RequestBody Account account) {
+        return as.saveAccount(account);
+    }
+
+    @PutMapping("accounts/{id}")
+    public Account updateAccount(@PathVariable Long id, @RequestBody Account updatedAccount) {
+        return as.updateAccount(id, updatedAccount);
+    }
+
+    @GetMapping("accounts/{id}")
+    public Account getAccountById(@PathVariable Long id) {
+        return as.getAccountById(id);
+    }
+
+    @DeleteMapping("accounts/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        as.deleteAccount(id);
     }
 }
