279e2b20699d1b21d14b0d37e17972d643cf6281
Rogier van Schaik <rogiervschaik@hotmail.com>
2023-08-09 13:26:32 +0200

    nieuwe commit

--- src/main/java/nl/framework/applicatie/persist/AccountService.java
+++ src/main/java/nl/framework/applicatie/persist/AccountService.java
@@ -4,55 +4,33 @@ import nl.framework.applicatie.domein.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class AccountService {
 
     @Autowired
     private AccountRepository ar;
-    public Iterable<Account> vindAlleAccounts(){
+
+    public Iterable<Account> vindAlleAccounts() {
         return ar.findAll();
     }
 
-    public Account opslaanAccount(Account account){
+    public Account saveAccount(Account account) {
         return ar.save(account);
     }
 
-    public void verwijderAccount(Account account) {
-        ar.delete(account);
+    public Account updateAccount(Account account) {
+        return ar.save(account);
     }
 
+    public void deleteAccount(Long id) {
+        ar.deleteById(id);
+    }
 
-    // public class AccountService {
-    //
-    //    private final Map<Long, Account> accountsMap = new HashMap<>();
-    //    private final AtomicLong accountIdGenerator = new AtomicLong(1);
-    //
-    //    public Iterable<Account> vindAlleAccounts() {
-    //        return accountsMap.values();
-    //    }
-    //
-    //    public Account saveAccount(Account account) {
-    //        long newId = accountIdGenerator.getAndIncrement();
-    //        account.setId(newId);
-    //        accountsMap.put(newId, account);
-    //        return account;
-    //    }
-    //
-    //    public Account updateAccount(Long id, Account updatedAccount) {
-    //        if (accountsMap.containsKey(id)) {
-    //            updatedAccount.setId(id);
-    //            accountsMap.put(id, updatedAccount);
-    //            return updatedAccount;
-    //        }
-    //        return null;
-    //    }
-    //
-    //    public Account getAccountById(Long id) {
-    //        return accountsMap.get(id);
-    //    }
-    //
-    //    public void deleteAccount(Long id) {
-    //        accountsMap.remove(id);
-    //    }
-    //b
+    public Account getAccountById(Long id) {
+        return ar.findById(id).orElse(null);
+    }
 }
